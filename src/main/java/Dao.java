4f4b8b3c502df7a5dbd6ec2df5398a4837329cec
242399 Grzybek Artur <242399@edu.p.lodz.pl>
2022-11-28 11:18:10 +0100

    Serializable w klasach, przy wielomodulowosci nie dziala jacoco

--- src/main/java/Dao.java
+++ src/main/java/Dao.java
@@ -1,3 +1,5 @@
+import java.io.Serializable;
+
 public interface Dao<T> {
     T read();
 
--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -1,3 +1,5 @@
-public interface SudokuSolver {
+import java.io.Serializable;
+
+public interface SudokuSolver extends Serializable {
     void solve(SudokuBoard board);
 }
